61d915af7c3a6e060d7a43ef7b07ad6869cb2527
Wicky2001 <pmwickramaratna@gmail.com>
2024-06-21 23:04:32 +0530

    frontend is modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,54 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Desktop from './Coponents/Pages/Desktop';
-import { io } from 'socket.io-client';
-const { v4: uuidv4 } = require('uuid');
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Desktop from "./Coponents/Pages/Desktop";
+import { io } from "socket.io-client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+const { v4: uuidv4 } = require("uuid");
 
 const App = () => {
-  const [ clientId] = useState(uuidv4());
-  const [message, setMessage] = useState('');
+  const [clientId] = useState(uuidv4());
+  const [message, setMessage] = useState("");
   const socket = useRef(null);
 
-  const [processComplete,setProcessComplete]=useState(false);
-
+  const [processComplete, setProcessComplete] = useState(false);
 
   useEffect(() => {
     // Initialize the socket connection
-    socket.current = io('http://localhost:5001', {
-      transports: ['websocket'], // Force WebSocket
+    socket.current = io("http://localhost:5001", {
+      transports: ["websocket"], // Force WebSocket
       reconnectionAttempts: 5, // Retry up to 5 times
       reconnectionDelay: 2000, // Wait for 2 seconds before attempting reconnection
     });
 
     // Define socket event handlers
     const setupSocketEventHandlers = () => {
-      socket.current.on('connect', () => {
-        console.log('Connected to the server with socket ID:', socket.current.id);
-        console.log('Registering user with ID:',  clientId);
-        socket.current.emit('register',  clientId);
+      socket.current.on("connect", () => {
+        console.log(
+          "Connected to the server with socket ID:",
+          socket.current.id
+        );
+        console.log("Registering user with ID:", clientId);
+        socket.current.emit("register", clientId);
       });
 
-      socket.current.on('processingComplete', (resultDir) => {
-        console.log('Processing complete with result directory:', resultDir);
+      socket.current.on("processingComplete", (resultDir) => {
+        console.log("Processing complete with result directory:", resultDir);
 
         setProcessComplete(true);
-       
       });
 
-      socket.current.on('disconnect', () => {
-        console.log('Disconnected from the server');
+      socket.current.on("disconnect", () => {
+        console.log("Disconnected from the server");
       });
 
-      socket.current.on('connect_error', (error) => {
-        console.error('Connection error:', error);
+      socket.current.on("connect_error", (error) => {
+        console.error("Connection error:", error);
       });
 
-      socket.current.on('reconnect_attempt', () => {
-        console.log('Attempting to reconnect...');
+      socket.current.on("reconnect_attempt", () => {
+        console.log("Attempting to reconnect...");
       });
     };
 
@@ -53,21 +56,27 @@ const App = () => {
 
     return () => {
       // Clean up the socket event handlers
-      console.log('Cleaning up socket listeners');
-      socket.current.off('connect');
-      socket.current.off('processingComplete');
-      socket.current.off('disconnect');
-      socket.current.off('connect_error');
-      socket.current.off('reconnect_attempt');
+      console.log("Cleaning up socket listeners");
+      socket.current.off("connect");
+      socket.current.off("processingComplete");
+      socket.current.off("disconnect");
+      socket.current.off("connect_error");
+      socket.current.off("reconnect_attempt");
       socket.current.disconnect();
     };
-  }, [ clientId]);
+  }, [clientId]);
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Desktop clientId={ clientId} processComplete={processComplete} />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Desktop clientId={clientId} processComplete={processComplete} />
+            }
+          />
         </Routes>
       </BrowserRouter>
       <div dangerouslySetInnerHTML={{ __html: message }} />
---
+++ frontend/src/Coponents/UploadForm.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { FaRegFilePdf, FaFileCircleCheck } from "react-icons/fa6";
+import { TbCloudCancel } from "react-icons/tb";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function UploadForm() {
+  const [markingSchemeFileSelected, setMarkingSchemeFileSelected] =
+    useState(false);
+  const [answerSheetFileSelected, setAnswerSheetFileSelected] = useState(false);
+  const [disableTextInput, setDisableTextInput] = useState(false);
+  const [disableFileInputs, setDisableFileInputs] = useState(false);
+
+  const changeFileUploadLogo = (fileSelected) => {
+    if (fileSelected) {
+      return <FaFileCircleCheck />;
+    } else if (disableFileInputs) {
+      return <TbCloudCancel />;
+    } else {
+      return <FaRegFilePdf />;
+    }
+  };
+
+  return (
+    <>
+      <div className="container upload-container">
+        <div className="row justify-content-center">
+          <form>
+            <div className="row">
+              <div className="col">
+                <div className="file-upload-wrapper">
+                  <input
+                    type="file"
+                    className="file-input"
+                    id="markingScheme"
+                    disabled={disableFileInputs}
+                    style={{ display: "none" }}
+                    onChange={(event) => {
+                      if (event.target.files.length > 0) {
+                        setMarkingSchemeFileSelected(true);
+                        setDisableTextInput(true); // When file is uploaded, disable the text inputs
+                      } else {
+                        setMarkingSchemeFileSelected(false);
+                        setDisableTextInput(false);
+                      }
+                    }}
+                  />
+                  <label htmlFor="markingScheme" className="file-label">
+                    {changeFileUploadLogo(markingSchemeFileSelected)}
+                  </label>
+                </div>
+                <input
+                  type="text"
+                  disabled={disableTextInput}
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Marking Scheme"
+                  onChange={(event) => {
+                    if (event.target.value.length > 0) {
+                      setDisableFileInputs(true);
+                    } else {
+                      setDisableFileInputs(false);
+                    }
+                  }}
+                />
+              </div>
+              <div className="col">
+                <div className="file-upload-wrapper">
+                  <input
+                    type="file"
+                    disabled={disableFileInputs}
+                    className="file-input"
+                    id="answerSheet"
+                    style={{ display: "none" }}
+                    onChange={(event) => {
+                      if (event.target.files.length > 0) {
+                        setAnswerSheetFileSelected(true);
+                        setDisableTextInput(true);
+                      } else {
+                        setAnswerSheetFileSelected(false);
+                        setDisableTextInput(false);
+                      }
+                    }}
+                  />
+                  <label htmlFor="answerSheet" className="file-label">
+                    {changeFileUploadLogo(answerSheetFileSelected)}
+                  </label>
+                </div>
+                <input
+                  type="text"
+                  disabled={disableTextInput}
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Answers"
+                  onChange={(event) => {
+                    if (event.target.value.length > 0) {
+                      setDisableFileInputs(true);
+                    } else {
+                      setDisableFileInputs(false);
+                    }
+                  }}
+                />
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UploadForm;
